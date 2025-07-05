bf1300762c311fb52582db3324cee78ef5b8346a
VoQuangBao <0306221104@caothang.edu.vn>
2025-07-05 21:25:59 +0700

    dang nhap sai 5 lan

--- app/Http/Controllers/Home/LoginController.php
+++ app/Http/Controllers/Home/LoginController.php
@@ -5,9 +5,18 @@ namespace App\Http\Controllers\Home;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
+    use AuthenticatesUsers, ThrottlesLogins;
+
+    public function username() 
+    {
+        return 'email';
+    }
+
     public function showLoginForm()
     {
         return view('homepage.login');
@@ -15,7 +24,22 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
+        $this->validateLogin($request);
+
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            $seconds = $this->limiter()->availableIn(
+                $this->throttleKey($request)
+            );
+             
+            return back()->withErrors(['email' => 'Bạn đã đăng nhập quá nhiều lần. Vui lòng thử lại sau ' . $seconds . ' giây ',]);
+        }
+
         $credentials = $request->only('email', 'password');
+
+        $this->incrementLoginAttempts($request);
+
 
         // Tìm user theo email
         $user = \App\Models\KhachHang::where('email', $credentials['email'])->first();
@@ -32,9 +56,10 @@ class LoginController extends Controller
                 Auth::logout();
                 return redirect()->route('login')->with('error', 'Tài khoản của bạn đã bị khóa.');
             }
+
+            $this->clearLoginAttempts($request);
     
             $user->update(['last_login_at' => now()]);
-
             // Gộp giỏ hàng
             (new \App\Http\Controllers\Home\CartController())->mergeCart();
 
@@ -57,6 +82,20 @@ class LoginController extends Controller
         ])->withInput();
     }
 
+    protected function throttleKey(Request $request)
+    {
+        return strtolower($request->input($this->username())) . '|' . $request->ip();
+    }
+
+    protected function decayMinutes()
+    {
+        return 1; // Thời gian khóa sau khi quá nhiều lần đăng nhập
+    }
+
+    protected function maxAttempts()
+    {
+        return 5; // Số lần đăng nhập tối đa trước khi bị khóa
+    }
 
     public function logout(Request $request)
     {
